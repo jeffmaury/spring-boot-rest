eae6690cd22be395d8067852ff4cb272b816858f
Jeff MAURY <jmaury@redhat.com>
2018-09-20 08:23:35 +0200

    Shorten package names (2)
    Signed-off-by: Jeff MAURY <jmaury@redhat.com>

--- src/main/java/com/redhat/sb/samples/sbr/SpringBootRestApplication.java
+++ src/main/java/com/redhat/sb/samples/sbr/SpringBootRestApplication.java
@@ -1,4 +1,4 @@
-package com.redhat.springboot.samples.sbr;
+package com.redhat.sb.samples.sbr;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/com/redhat/sb/samples/sbr/SpringBootRestApplicationTests.java
+++ src/test/java/com/redhat/sb/samples/sbr/SpringBootRestApplicationTests.java
@@ -1,4 +1,4 @@
-package com.redhat.springboot.samples.springbootrest;
+package com.redhat.sb.samples.sbr;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
