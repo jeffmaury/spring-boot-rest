70be15aee6201968a5472286ecda0f6f60d2dd1b
Jeff MAURY <jmaury@redhat.com>
2018-09-20 07:46:21 +0200

    Initial version
    Signed-off-by: Jeff MAURY <jmaury@redhat.com>

---
+++ src/main/java/com/redhat/springboot/samples/springbootrest/SpringBootRestApplication.java
@@ -0,0 +1,12 @@
+package com.redhat.springboot.samples.springbootrest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootRestApplication.class, args);
+	}
+}
